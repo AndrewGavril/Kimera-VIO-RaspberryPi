8841d41d016364aed520bd5022d793e1640843c7
Andrew <gavrilov.andrew1999@yandex.ru>
2020-07-20 16:41:13 +0300

    Adding full script for prf testing and generating svg

--- get_flame_gr.py
+++ get_flame_gr.py
@@ -1,21 +1,46 @@
 import subprocess
 import time
+import psutil
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 path_to_lib = "../dependences/FlameGraph/"
 if path_to_lib[-1] != '/':
     path_to_lib += "/"
 
-p = subprocess.Popen(["./scripts/stereoVIOEuroc.bash", "-p", "/home/andrew/work/kimera_project/V1_01_easy"])
-pid = p.pid
-time.sleep(0.5)
-perf = subprocess.call(["perf", "record", "-F", "99", "-a", "-g", "--", "sleep", "30"])
+p = subprocess.Popen("./scripts/stereoVIOEuroc.bash -p /home/andrew/work/kimera_project/V1_01_easy".split())
+time.sleep(1)
+current_process = psutil.Process()
+kimera_process_id = int(current_process.children(recursive=True)[1].pid)
+pids = []
+print((bcolors.OKGREEN + "{}" + bcolors.ENDC).format(kimera_process_id))
+
+with open("stereoVioPids.txt", "w+") as file:
+    pstree = subprocess.call(["pstree", "-p", str(kimera_process_id)], stdout=file)
+
+with open("stereoVioPids.txt", "r") as file:
+    for line in file:
+        pid = line[line.find("{stereoVIOEuroc}(") + len("{stereoVIOEuroc}("):-2]
+        pids.append(pid)
+
+pids = ",".join(pids)
+print(pids)
+
+perf = subprocess.Popen("perf record -F 99 -p {} -g -- sleep 15".format(pids).split())
+perf.wait()
 p.wait()
 
-with open("out.perf", "w") as file:
-    print("a")
+with open("out.perf", "w+") as file:
     p = subprocess.call("perf script".split(), stdout=file)
-with open("out.folded", "w") as file:
+with open("out.folded", "w+") as file:
     p = subprocess.call((path_to_lib + "stackcollapse-perf.pl out.perf").split(), stdout=file)
-with open("kernel.svg", "w") as file:
+with open("kernel.svg", "w+") as file:
     p = subprocess.call((path_to_lib + "flamegraph.pl out.folded").split(), stdout=file)
